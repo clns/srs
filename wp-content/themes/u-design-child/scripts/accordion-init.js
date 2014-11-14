c2584b6abd7d2ca6d1fb86f718e633b876fc31aa
eh11fx <eric.hall@mobilization.org>
2014-11-14 16:05:44 -0600

    Update franchise bootcamp landing page accordion initialization.

--- wp-content/themes/u-design-child/scripts/accordion-init.js
+++ wp-content/themes/u-design-child/scripts/accordion-init.js
@@ -1,3 +1,12 @@
 /**
- * Created by EricHall on 11/13/2014.
+ * Created by Eric Hall on 11/13/2014.
  */
+
+(function($) {
+    $(window).load(function() {
+        $(".expanding-heading").on('click', function(){
+            $(this).next('.expanding-content').slideToggle();
+            $(this).children('.expanding-icon').toggleClass('expanding-icon-plus expanding-icon-minus');
+        });
+    });
+})(jQuery)
