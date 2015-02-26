5bdab60aab1fdf4cb6a172d0dcf958b11c64ea76
Malachi Wurpts <admin@mobilization.org>
2015-02-26 17:01:01 -0600

    Moved Registration Report Link custom field from post to faciliator taxonomy because there is now only one report link per franchise. This way we don't need to copy the link in all posts.

--- wp-content/themes/u-design-child/taxonomy-facilitator.php
+++ wp-content/themes/u-design-child/taxonomy-facilitator.php
@@ -27,12 +27,23 @@ $content_position = 'grid_24';
         <div id="main-content" class="<?php echo $content_position; ?>">
                 <div class="grid_24 page-facilitator-participant clearfix">
                     <div class="grid_12" style="font-color: #333333;">
-                        <div style="padding-top: 80px;">
+                        <div style="padding-top: 80px; padding-bottom: 80px;">
                             <h1 style="font-size: 26px; line-height: 34px; color: #333333; margin-top: 0px;">Bootcamp Facilitator Portal</h1>
-                            <h2 style="font-size: 18px !important; font-weight: 500 !important; color: #000; margin-top: 70px;">Registration Reports</h2>
-                            <p style="font-size: 16px; display: inline-block; padding-right: 20px; padding-top: 20px;"><?php echo get_post_meta($post->ID, 'location_city', true) . ', ' . get_post_meta($post->ID, 'location_state', true); ?> Bootcamp</p>
+                            <h2 style="font-size: 18px !important; font-weight: 500 !important; color: #000; margin-top: 50px; margin-bottom: 20px;">Registration Reports</h2>
                             <div style="width: 115px; height: 35px; line-height: 22px; margin: 0 auto; text-align: center; background: #F1632A; display: inline-block;">
-                                <p style="text-align: center; color: #ffffff; font-size: 16px;"><a href="<?php echo get_post_meta($post->ID, 'registration_report_link', true); ?>" style="color:#FFF;">View Report</a></p>
+                            
+								<?php
+								// Get the registration_report_link custom field which was added to the faciliator taxonomy using PODs 
+								$args = array('hide_empty' => 0);                            
+								$taxterms = get_terms('facilitator', $args);
+								$podterms = pods('facilitator');
+								foreach($taxterms as $taxterm) :
+									$podterms->fetch($taxterm->term_id);
+									$reportlink = $podterms->get_field('registration_report_link');
+								endforeach;
+								?>                            
+                            
+                                <p style="text-align: center; color: #ffffff; font-size: 16px;"><a href="<?php echo $reportlink; ?>" style="color:#FFF;">View Reports</a></p>
                             </div>
                         </div>
                     </div>
