5340190f5aebeebd4037bc40d4dc730d26add529
Malachi Wurpts <admin@mobilization.org>
2015-02-26 16:59:15 -0600

    Cleaned up indents. Not sure this file is used.

--- wp-content/themes/u-design-child/page-FranchiseFacilitator.php
+++ wp-content/themes/u-design-child/page-FranchiseFacilitator.php
@@ -25,22 +25,24 @@ $content_position = 'grid_24';
     <div id="content-container" class="container_24" style="padding: 0;">
         <div id="main-content" class="<?php echo $content_position; ?>">
             <div>
-                <?php       udesign_main_content_top( is_front_page() ); ?>
+                <?php udesign_main_content_top( is_front_page() ); ?>
 
-                <?php	    if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                    <?php               udesign_entry_before(); ?>
-                    <div class="entry" style="padding-top: 0";>
-                    <?php                   udesign_entry_top(); ?>
-                    <?php			the_content(__('<p class="serif">Read the rest of this page &raquo;</p>', 'udesign')); ?>
-                    <?php                   udesign_entry_bottom(); ?>
-                </div>
-                <?php               udesign_entry_after(); ?>
-            </div>
-            <?php		( $udesign_options['show_comments_on_pages'] == 'yes' ) ? comments_template() : '';
-            endwhile; endif; ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+	                    <?php udesign_entry_before(); ?>
+	                    <div class="entry" style="padding-top: 0";>
+	                    	<?php 
+	                    	udesign_entry_top();
+	                    	the_content(__('<p class="serif">Read the rest of this page &raquo;</p>', 'udesign'));
+	                    	udesign_entry_bottom(); 
+	                    	?>
+	                	</div>
+	                	<?php udesign_entry_after(); ?>
+	            	</div>
+	            	<?php ( $udesign_options['show_comments_on_pages'] == 'yes' ) ? comments_template() : '';
+            	endwhile; endif; ?>
             <div class="clear"></div>
-            <?php	    udesign_main_content_bottom(); ?>
+            <?php udesign_main_content_bottom(); ?>
         </div><!-- end main-content-padding -->
     </div><!-- end main-content -->
 
