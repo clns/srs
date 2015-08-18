28d32022591d52af5bfc43985058d06917c598fd
joshua.smith <joshua.smith@mobilization.org>
2015-08-18 12:09:56 -0500

    The catapult series page and the CSS for it.

--- wp-content/themes/u-design-child/page-Catapult-Series.php
+++ wp-content/themes/u-design-child/page-Catapult-Series.php
@@ -5,42 +5,76 @@ Template Name: Catapult Series
 
 
 get_header(); ?>
-<?php
-$custom_terms = get_terms('catapulttopic');?>
-<div class = container>
-    <?php
-        foreach($custom_terms as $custom_term) {
-            wp_reset_query();
 
-            $args = array(
-              'post_type'      => 'catapultvideo',
-              'posts_per_page' => -1,
-            );
+    <div id="content-container" class="full-width">
+        <div id="main-content" class="full-width">
+            <div id="network" class="network-page catapult">
+                <div class="page-banner">
+                    <div class="banner-text"><?php echo wp_title("",true); ?></div>
+                </div>
 
-            $loop = new WP_Query($args);
+                <div class="intro-text">
+                    <?php
+                    #This section generates the blurb under the page-banner
+                    $custom_terms = get_terms('catapulttopic');
+                    wp_reset_query();
+                    $args = array(
+                        'post_type'      => 'catapultvideo',
+                        'posts_per_page' => -1,
+                    );
 
-            if ($loop->have_posts()) { ?>
+                    $loop = new WP_Query($args);
 
-                <header class="catapult-video-title">
-                    <?php echo '<h2 style = "font-size: 18px; color: black">' . $custom_term->name . '</h2>'; ?>
-                </header>
+                    if ($loop->have_posts()) {
+                            echo the_content();
+                        }
+                    wp_reset_postdata();
+                    ?>
+                </i></div>
+
+
                 <?php
-                while ($loop->have_posts()) : $loop->the_post(); ?>
-                    <div class="catapult-video-content">
-                        <?php
-                        if(has_term($custom_term->name, 'catapulttopic')){
-                            echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';
-                            the_content();}
-                            ;?>
+                $custom_terms = get_terms('catapulttopic');?>
+                <?php
+                foreach($custom_terms as $custom_term) {
+                wp_reset_query();
+
+                $args = array(
+                    'post_type'      => 'catapultvideo',
+                    'posts_per_page' => -1,
+                );
+
+                $loop = new WP_Query($args);
+
+                if ($loop->have_posts()) {;?>
+                <div class="previews-container-catapult">
+                    <div class="previews-border"></div>
+                    <div class="section-header">
+                        <?php echo $custom_term->name; ?>
                     </div>
-                <?php
-                endwhile;
-            }
-            else{
-                '<p style = "font-size: 54px; color: black"> There is nothing here </p>';
-            }
-        }
-        wp_reset_postdata();
-    ?>
-</div>
+                    <?php
+                    while ($loop->have_posts()) : $loop->the_post(); ?>
+                        <div class="catapult-video-content">
+                            <?php
+                            if(has_term($custom_term->name, 'catapulttopic')){
+                                echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';
+                                the_content();
+                                }
+                                ;?>
+                        </div>
+                    <?php
+                    endwhile;
+                    }
+                    ?></div><?php
+                 }
+                wp_reset_postdata();
+                ?>
+
+            </div>
+            </div>
+        </div><!-- end main-content -->
+    </div><!-- end content-container -->
+
+    <div class="clear"></div>
+
 <?php get_footer(); ?>
--- wp-content/themes/u-design-child/single-catapultvideo.php
+++ wp-content/themes/u-design-child/single-catapultvideo.php
@@ -1,12 +1,14 @@
 <?php
 
 get_header(); ?>
-<div style="position: relative; left: 625px; margin: 30px auto 30px auto;">
+<div class="catapult-video-container">
+<div class="catapult-video" >
     <?php
     $customdata = get_post_meta($post->ID, 'vimeo_video_id', true);
     if( !empty($customdata) ){
         ?><iframe src="https://player.vimeo.com/video/<?php echo $customdata;?>"
                   width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allow fullscreen></iframe>
     <?php }?>
+</div>
 </div>
 <?php get_footer(); ?>
