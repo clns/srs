ca17dc6bf8fabf0a6bb6e462e3d92c9dca9883ac
Malachi Wurpts <admin@mobilization.org>
2016-02-15 15:50:43 -0600

    Moved all content on this page to the post content so the SRS team can edit it.

--- wp-content/themes/u-design-child/page-Network.php
+++ wp-content/themes/u-design-child/page-Network.php
@@ -11,73 +11,18 @@ if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 get_header();
 ?>
 <div id="content-container" class="full-width">
-	<div id="main-content" class="full-width">
-		<div id="network">
-			<div class="page-banner"></div>
-      <div class="page-subtitle">Membership options made with you in mind</div>
-      <div class="page-container">
-        <div class="member-container">
-          <div class="member-splash">
-            <div class="label">Become a Network Member</div>
-            <a href="network-membership"><button>Learn more</button></a>
-          </div>
-          <div class="description">
-            <span>When you become a member</span> of the SRS Network, you become
-            part of a vibrant community all over the world. Get access to 
-            exclusive resources and training opportunities, helping you 
-            create a culture of a fully funded ministry.
-          </div>
-          <div class="details">
-            <div class="detail-heading">Membership Benefits</div>
-            <ul>
-              <li>$150 off registration to the annual <a href="srs-national-conference">National Conference</a></li>
-              <li>Access to monthly <a href="webinar">webinars</a></li>
-              <li><a href="audit">SRS Audit Survey</a></li>
-              <li>Access to over 250 <a href="catapult">Catapult Series</a> videos</li>
-            </ul>
-            <hr>
-            <div class="detail-heading">Who Can Join</div>
-            <ul>
-              <li>Organization Leaders</li>
-              <li>Coaches</li>
-              <li>Support Raising Trainers</li>
-            </ul>
-          </div>
-        </div>
-        
-        <div class="fac-container">
-          <div class="fac-splash">
-            <div class="label">Become a Facilitator</div>
-            <a href="facilitator-membership"><button>Learn more</button></a>
-          </div>
-          <div class="description">
-            <b>Ready to take the next step</b> and facilitate your own
-            SRS Bootcamp? Become a certified Facilitator and get access 
-            to exclusive resources and training opportunities.
-          </div>
-          <div class="details">
-            <div class="detail-heading">Membership Benefits</div>
-            All Network Member benefits plus
-            <ul>
-              <li>SRS Bootcamp <a href="facilitatorstraining">Facilitator Training</a></li>
-              <li><i>Bootcamp in a Box</i> setup materials</li>
-              <li><i>Free</i> registration to <a href="srs-national-conference">Annual National Conference</a></li>
-              <li>Discounted additional facilitator memberships</li>
-            </ul>
-            <hr>
-            <div class="detail-heading">Required Qualifications</div>
-            <ul>
-              <li>Participate in a public SRS Bootcamp (2014 or later)</li>
-              <li>Participate in an SRS <a href="facilitatorstraining">Facilitator Training</a></li>
-              <li>Attend the annual <a href="srs-national-conference">SRS National Conference</a> each year</li>
-              <li>Live and minister on personal support</li>
-            </ul>
-          </div>
-        </div>
-        <div class="clear"></div>
-      </div>
-		</div>
-	</div><!-- end main-content -->
+  <div id="main-content" class="full-width">
+    <div id="network">
+      <div class="page-banner"></div>
+
+      <?php
+      # This section generates the blurb under the title
+      the_post();
+      the_content();
+      ?>
+
+    </div>
+  </div><!-- end main-content -->
 </div><!-- end content-container -->
 
 <div class="clear"></div>
