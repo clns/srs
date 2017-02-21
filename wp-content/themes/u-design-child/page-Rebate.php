aa92d075069249a4631631575cfe3842e8a57335
Malachi Wurpts <admin@mobilization.org>
2017-02-21 15:51:23 -0600

    refs #6553 Update to use rebate link and fix padding

--- wp-content/themes/u-design-child/page-Rebate.php
+++ wp-content/themes/u-design-child/page-Rebate.php
@@ -10,19 +10,8 @@ if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 get_header();
 
-//$today = date("Y-m-d");
-//$date1 = strtotime('30 days ago');
-//$date2 = date("Y-m-d");
-
-$date1 = date("Ymd", strtotime('-30 day'));
+$date1 = date("Ymd", strtotime('-8 day'));
 $date2 = date("Ymd", strtotime('today'));
-
-//$date1 = date("Ymd", strtotime('2016-01-01'));
-//$date2 = date("Ymd", strtotime('2017-09-18'));
-
-//$date1 = '2016-01-01';
-//$date2 = '2017-09-18';
-
 
 $args = array(
   'post_type'  => 'bootcamp',
@@ -34,32 +23,11 @@ $args = array(
   'meta_query' => array(
     array(
       'key'     => 'end_date',
-      //'value'   => array(date('d/m/Y'), date('d/m/Y', strtotime('28 days'))),
       'value'   =>  array($date1,$date2),
       'type'    => 'DATE',
       'compare' => 'BETWEEN'
     )
   )
-
-//  'meta_query' => array(
-//    'relation' => 'AND',
-//    array(
-//      'key'     => 'end_date',
-//      //'value'   => date("Y-m-d H:i:s"),
-//      //'value'   => '2016-01-01',
-//      'value'   => $date1,
-//      'compare' => '>=',
-//      'type'    => 'DATE'
-//    ),
-//    array(
-//      'key'     => 'end_date',
-//      //'value'   => date("Y-m-d H:i:s"),
-//      //'value'   => '2017-09-18',
-//      'value'   => $date2,
-//      'compare' => '<=',
-//      'type'    => 'DATE'
-//    )
-//  )
 
 );
 $the_query = new WP_Query($args);
@@ -75,10 +43,7 @@ $the_query = new WP_Query($args);
       the_content();
       ?>
 
-
-
     </div>
-
 
     <div id="page-franchise-participant">
 
@@ -121,7 +86,7 @@ $the_query = new WP_Query($args);
           }
           ?>
           <div class="bc<?php echo get_the_ID() ?> bootcampInfoBlock button-container" <?php echo $display ?>>
-            <p class="button-text"><?php echo '<a href="' . get_post_meta($post->ID, 'registration_link', true) . '">Request Rebate</a>'; ?></p>
+            <p class="button-text"><?php echo '<a href="' . get_post_meta($post->ID, 'rebate_link', true) . '">Request Rebate</a>'; ?></p>
           </div>
         <?php endwhile; wp_reset_postdata(); // end of the loop. ?>
       </div>
