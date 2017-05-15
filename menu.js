3ba77281177d1e8eea3690b173d94937ffd9ab6b
Calin Seciu <calinseciu@gmail.com>
2017-05-15 20:03:53 +0300

    Add html pages

---
+++ menu.js
@@ -0,0 +1,6 @@
+var flyoutToggleBtn = document.querySelector('.top-nav-bar-toggle');
+flyoutToggleBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    var flyout = document.querySelector('.top-nav-bar-flyout');
+    flyout.classList.toggle('open');
+});
