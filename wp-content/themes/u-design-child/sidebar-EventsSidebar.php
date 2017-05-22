e6c075bb86483d63f613960d5730ad17d17e0b56
Dan Seciu <dan@seciu.com>
2017-05-22 11:17:36 +0300

    Add EventsSidebar used in the Events page

---
+++ wp-content/themes/u-design-child/sidebar-EventsSidebar.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage U-Design-Child
+ */
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+?>
+
+<?php
+global $udesign_options;
+$sidebar_position = ($udesign_options['events_sidebar'] == 'left') ? 'grid_8 pull_16 sidebar-box' : 'grid_8';
+?>
+<div id="sidebar" class="<?php echo $sidebar_position; ?>">
+    <div id="sidebarSubnav">
+        <?php udesign_sidebar_top(); ?>
+
+        <?php // Widgetized sidebar
+        if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('EventsSidebar')) : ?>
+
+            <div class="custom-formatting">
+                <h3><?php esc_html_e('About This Sidebar', 'udesign'); ?></h3>
+                <ul>
+                    <?php _e("To edit this sidebar, go to admin backend's <strong><em>Appearance -> Widgets</em></strong> and place widgets into the <strong><em>Pages Sidebar 2</em></strong> Widget Area", 'udesign'); ?>
+                </ul>
+            </div>
+
+        <?php endif; ?>
+
+        <?php udesign_sidebar_bottom(); ?>
+    </div><!-- end sidebarSubnav -->
+</div><!-- end sidebar -->
+
+
