e904f8b6c1f844ca08de683cdaa2efaf0a39708f
Dan Seciu <dan@seciu.com>
2017-05-22 11:17:12 +0300

    Add Events page and additional styling

--- wp-content/themes/u-design-child/page-Events.php
+++ wp-content/themes/u-design-child/page-Events.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package WordPress
- * @subpackage U-Design
+ * @subpackage U-Design-Child
  */
 /**
  * Template Name: Events
@@ -10,104 +10,151 @@ if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 get_header();
 
-// Begin main posts' loop stuff here
-$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
 
-$global_posts_query = new WP_Query(
-    array(
-        'post_type' => 'webinar',
-        'post_status' => array(
-            'publish',
-            'private',
-        ),
-        'meta_query' => array(
+$args = array(
+    'post_type' => array('conference', 'bootcamp', 'webinar'),
+    'orderby' => 'meta_value',
+    'order' => 'ASC',
+    'posts_per_page' => 5,
+    'paged' => $paged,
+    'meta_query' => array(
+        array(
+            'relation' => 'AND',
             array(
-                'key' => 'webinar_date',
-                'compare' => '>=',
-                'type' => 'DATE',
-            )
+                'relation' => 'OR',
+                array('key' => 'start_date',),
+                array('key' => 'webinar_date',),
+            ),
+            array(
+                'relation' => 'OR',
+                array(
+                    'key' => 'conferences_status',
+                    'value' => array('Open', 'Closed'),
+                    'compare' => 'IN',
+                ),
+                array(
+                    'key' => 'bootcamp_status',
+                    'value' => array('Open', 'Closed'),
+                    'compare' => 'IN',
+                ),
+                array(
+                    'key' => 'webinar_date',
+                    'value' => current_time('Y-m-d'),
+                    'compare' => '>=',
+                    'type' => 'DATE',
+                ),
+            ),
         ),
-        'meta_key' => 'webinar_date',
-        'orderby' => 'meta_value',
-        'order' => 'DESC',
-        'paged' => $paged,
-        'posts_per_page' => 5
-    )
+    ),
+    'tax_query' => array(
+        'relation' => 'OR',
+        array( // Apply "srs" franchise taxonomy condition (applies to bootcamp and conference)
+            'taxonomy' => 'franchise',
+            'field'    => 'slug',
+            'terms' => array('srs', '', null),
+        ),
+        array( // This condition is needed in case it's a webinar, which doesn't have a franchise
+            'taxonomy' => 'franchise',
+            'field'    => 'id',
+            'operator' => 'NOT EXISTS',
+        ),
+    ),
 );
+$the_query = new WP_Query($args);
 ?>
 
-<div id="content-container" class="full-width">
-	<div id="main-content" class="full-width">
+    <div class="page-content">
+        <div class="left-side">
+            <div class="content-group">
 
-      <?php
-      // Display content of page
-      the_post();
-      the_content();
-      ?>
+                <div class="title">
+                    <h2>Events</h2>
+                </div>
 
-
-		<div id="network" class="network-page webinar">
-        <div class="title">
-            <h2>Events</h2>
-        </div>
-
-        <div class="events-grid">
-
-            <div class="header">
-                <div class="name">Event</div>
-                <div class="date">Date</div>
-                <div class="location">Location</div>
-            </div>
-
-
-
-            <?php
-
-
-                if($global_posts_query->have_posts()) :
-                    while($global_posts_query->have_posts()) : $global_posts_query->the_post(); ?>
-
-                        <div class="row">
-                            <div class="name">Bootcamp</div>
-                            <div class="date">Jan 12</div>
-                            <div class="location">Lubbock, TX</div>
-                            <div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</div>
+                <div class="events-grid">
+                    <div class="header">
+                        <div class="name"><?php _e('Event', 'u-design') ?></div>
+                        <div class="date"><?php _e('Date', 'u-design') ?></div>
+                        <div class="location"><?php _e('Location', 'u-design') ?></div>
+                    </div>
+                    <?php if ( $the_query->have_posts() ) while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                        <?php $is_webinar = 'webinar' == get_post_type(); ?>
+                        <div class="row" id="event-<?php the_ID(); ?>">
+                            <div class="name"><?php echo ucfirst(get_post_type()); ?></div>
+                            <div class="date">
+                                <?php if(!$is_webinar) {
+                                    $start_date_raw = strtotime(get_post_meta(get_the_ID(), 'start_date',true));
+                                    $end_date_raw = strtotime(get_post_meta(get_the_ID(), 'end_date',true));
+                                    $start_date = date("F j", $start_date_raw);
+                                    if (date("m", $start_date_raw) != date("m", $end_date_raw)) {
+                                        $end_date = date("F j", strtotime(get_post_meta(get_the_ID(), 'end_date',true)));
+                                    } else {
+                                        $end_date = date("j", strtotime(get_post_meta(get_the_ID(), 'end_date',true)));
+                                    }
+                                    echo $start_date . '-' . $end_date;
+                                } else {
+                                    echo date("F j, Y", strtotime(get_post_meta(get_the_ID(), 'webinar_date', true)));
+                                }?>
+                            </div>
+                            <div class="location">
+                                <?php
+                                    if(!$is_webinar) {
+                                        echo get_post_meta(get_the_ID(), 'location_city', true) . ', ' . get_post_meta(get_the_ID(), 'location_state', true);
+                                    } else {
+                                        echo get_post_meta(get_the_ID(), 'webinar_time', true);
+                                    }
+                                ?>
+                            </div>
+                            <div class="description">
+                                <?php if(!$is_webinar) {
+                                    echo wp_trim_words(get_post_meta(get_the_ID(), 'welcome_message', true), 10);
+                                } else {
+                                    echo wp_trim_words(get_post_meta(get_the_ID(), 'video_description', true), 10);
+                                } ?>
+                            </div>
                             <div class="link">
-                                <a href="">Learn More...</a>
+                                <?php $learn_more_url = get_permalink();
+                                if ("bootcamp" == get_post_type()) {
+
+                                    $terms = get_the_terms(get_the_ID(), 'franchise');
+                                    if ( $terms && !is_wp_error( $terms ) ) {
+                                        foreach ( $terms as $term ) {
+                                            $learn_more_url = get_term_link( $term->slug, 'franchise') . "?bootcampID=bc" . get_the_ID();
+                                        }
+                                    }
+                                    
+                                    $learn_more_url = get_term_link('srs', 'franchise') . "?bootcampID=bc" . get_the_ID();
+                                } ?>
+                                <a href="<?php echo $learn_more_url; ?>"><?php _e('Learn More...', 'u-design') ?></a>
                             </div>
                         </div>
+                    <?php endwhile; wp_reset_postdata(); // end of the loop. ?>
+                </div><!-- end events-grid -->
 
-                        <div class="clear"></div>
-                    <?php endwhile;
+                <div class="clear"></div>
 
-                    // Pagination
-                    if(function_exists('wp_pagenavi')) :
-
-                        wp_pagenavi( array( 'query' => $global_posts_query ) );
+                <div class="paging-toolbar">
+                    <?php // Pagination
+                    if (function_exists('wp_pagenavi')) :
+                        wp_pagenavi(array('query' => $the_query));
                     else : ?>
                         <div class="navigation">
                             <div class="alignleft"><?php previous_posts_link() ?></div>
                             <div class="alignright"><?php next_posts_link() ?></div>
                         </div>
-                    <?php
-                    endif;
+                    <?php endif; wp_reset_postdata(); ?>
+                </div>
 
-                    // Restore original Post Data
-                    wp_reset_postdata(); ?>
+            </div><!-- end content-group -->
+        </div><!-- end left-side -->
 
-                <?php else : ?>
-                    <h2 class="center"><?php esc_html_e('Not Found', 'udesign'); ?></h2>
-                    <p class="center"><?php esc_html_e("Sorry, but you are looking for something that isn't here.", 'udesign'); ?></p>
-                    <?php		get_search_form();
-                endif; ?>
-
-
+        <div class="right-side">
+            <?php if (sidebar_exist('EventsSidebar')) {
+                get_sidebar('EventsSidebar');
+            } ?>
         </div>
-    </div>
-  </div><!-- end main-content -->
-</div><!-- end content-container -->
-    <div class="clear"></div>
+        
+    </div><!-- end page-content -->
 
-
-<?php
-get_footer();?>
+<?php get_footer(); ?>
