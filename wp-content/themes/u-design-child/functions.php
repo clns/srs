8d65b244bef6f252ef6216a0af683f386e2a4b7f
Dan Seciu <dan@seciu.com>
2017-06-07 08:36:16 +0300

    Add NetworkSidebar to be used on the Network page

--- wp-content/themes/u-design-child/functions.php
+++ wp-content/themes/u-design-child/functions.php
@@ -304,6 +304,14 @@ if ( function_exists('register_sidebar') ) {
         'before_title' => '<h3 class="widgettitle">',
         'after_title' => '</h3>',
     ));
+    register_sidebar(array(
+        'name' => 'NetworkSidebar',
+        'description' => esc_html__('A widget area, used as a sidebar for the NetworkWithSidebar template.', 'udesign'),
+        'before_widget' => '<div id="%1$s" class="widget %2$s substitute_widget_class">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>',
+    ));
 
 }
 
---
+++ wp-content/themes/u-design-child/sidebar-NetworkSidebar.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage U-Design-Child
+ */
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+?>
+
+<div id="sidebar">
+    <div id="sidebarSubnav">
+        <?php udesign_sidebar_top(); ?>
+
+        <?php // Widgetized sidebar
+        if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('UpcomingEvents')) : ?>
+
+            <div class="custom-formatting">
+                <h3><?php esc_html_e('About This Sidebar', 'udesign'); ?></h3>
+                <ul>
+                    <?php _e("To edit this sidebar, go to admin backend's <strong><em>Appearance -> Widgets</em></strong> and place widgets into the <strong><em>UpcomingEvents</em></strong> Widget Area", 'udesign'); ?>
+                </ul>
+            </div>
+
+        <?php endif; ?>
+
+        <?php udesign_sidebar_bottom(); ?>
+    </div><!-- end sidebarSubnav -->
+</div><!-- end sidebar -->
+
+
